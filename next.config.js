4ff7a97f5e08553923ad6190df157c17b9843901
Devin Vasavong <devin@vasavong.com>
2023-07-30 20:31:08 -0700

    Added next-fonts to next.config.json

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+const withFonts = require('next-fonts')
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = withFonts({
+    webpack(nextConfig, options) {
+        return nextConfig
+    }
+})

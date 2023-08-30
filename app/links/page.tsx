9c8c5fe39f5925749f3c0d5df8443ec7be0ac69f
Devin Vasavong <devin@vasavong.com>
2023-08-30 16:25:16 -0400

    Added caption

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -4,16 +4,19 @@ import Link from "next/link"
 
 function Card({
     title,
-    link
+    link,
+    caption
 } : {
     title: String,
-    link: string
+    link: string,
+    caption?: string
 }) {
     return (
-        <a href={link} aria-label={`Link to ${title}`}>
+        <a href={link} aria-label={`Link to ${title}`} className="group">
             <div className="text-center bg-gray-50 hover:bg-gray-200 rounded-lg transition-colors duration-300 py-3">
                 {title}
             </div>
+            { caption && <p className="text-opacity-0 hidden group-hover:block group-hover:text-opacity-100 text-xs text-center pt-3 transition-all duration-300">{caption}</p>}
         </a>
     )
 }
@@ -23,10 +26,10 @@ export default function Links() {
         <div>
             <Navbar />
             <div className="mx-auto max-w-5xl p-10">
-                <div className="grid-cols-1 flex flex-col gap-2">
+                <div className="grid-cols-1 flex flex-col gap-2 transition-all duration-300">
                     <Card title="Read.cv" link="https://read.cv/cdv" />
                     <Card title="Twitter" link="https://twitter.com/devinvasavong" />
-                    <Card title="LinkedIn" link="https://linkedin.com/in/devinvasavong" />
+                    <Card title="LinkedIn" link="https://linkedin.com/in/devinvasavong" caption="Connect with me" />
                     <Card title="Instagram" link="https://instagram.com/cdvasavong" />
                 </div>
             </div>

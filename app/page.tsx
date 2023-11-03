32cedbfcbc90f3a776eafd63ebc1667163818097
Devin Vasavong <devin@vasavong.com>
2023-11-03 12:37:19 -0400

    Started to work on portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -10,21 +10,64 @@ export default function Main() {
     return (
         <div className="px-10 w-screen sm:max-w-6xl mx-auto">
             <Navbar />
-            <motion.div className="py-10">
+            <motion.div className="py-10" initial={{ y: 5, opacity: 0, }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 1 }} >
                 <p>
-                    hi, my name is devin vasavong. i&#39;m a second year software engineering student at{' '}
+                    Hi, my name is Devin Vasavong. I&#39;m a second year software engineering student at{' '}
                     <Link aria-label="link to rit website" href="https://rit.edu" target="_blank" className="text-[#F76902] font-semibold hover:underline">Rochester Institute of Technology.</Link>
                     <br/>
                     <br/>
-                    i have a strong passion for creating side projects and love to work in a collaborative team. i am currently working on a
-                    teleprompting and hrms service. both of which are created using next.js
+                    I have a strong passion for creating side projects and love to work in a collaborative team. I am currently working on a
+                    Teleprompting and HRMS service. Both of which are created using Next.js!
                     <br/>
                     <br/>
-                    as of october 11, 2023, i am looking for a summer/fall 2024 co-op in full-stack development. if i seem to fit into this criteria, please contact me using the email listed above.
+                    As of October 11, 2023, I am looking for a summer/fall 2024 co-op in full-stack development. If I seem to fit into this criteria, please contact me using the email listed above.
                     <br/>
                     <br/>
-                    if there are any questions regarding my skills, projects, or anything else, please contact me using the email listed above as well.
+                    If there are any questions regarding my skills, projects, or anything else, please contact me using the email listed above as well.
                 </p>
+            </motion.div>
+            <motion.div className="py-10" initial={{ y: 5, opacity: 0, }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 1.5 }} >
+                <h1 className="font-bold pb-3 text-lg">Experience</h1>
+                <div className="space-y-3">
+                    <div className="flex justify-between items-start">
+                        <div>
+                            <p className="font-medium">Customer Service Representative</p>
+                            <p className="text-sm text-gray-400">Wegmans Food Markets</p>
+                        </div>
+                        <p className="text-gray-400 text-sm">March 2021 - Present</p>
+                    </div>
+                    <div>
+                        <ul className="list-disc list-inside text-sm">
+                            <li>Provided 100% customer satisfaction</li>
+                            <li>Resolve customer concerns, comments, and suggestions</li>
+                            <li>Making phone calls to departments to confirm inventory and item details</li>
+                            <li>Creating and updating customer profiles on Customer Loyalty Management (CLM) software</li>
+                        </ul>
+                    </div>
+                </div>
+            </motion.div>
+            <motion.div className="py-10" initial={{ y: 5, opacity: 0, }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 2 }} >
+                <h1 className="font-bold pb-3 text-lg">Projects</h1>
+                <div className="space-y-3">
+                    <div className="flex justify-between items-start">
+                        <div>
+                            <p className="font-medium">Workplace Scheduling (Shift List)</p>
+                            <p className="text-sm text-gray-400">Personal Project + Collaborator</p>
+                        </div>
+                        <Link href="/work/workplace-scheduling" aria-label="link to workplace scheduling project page">
+                            <p className="text-gray-400 text-sm hover:underline cursor-pointer">View Project</p>
+                        </Link>
+                    </div>
+                    <div className="flex justify-between items-start">
+                        <div>
+                            <p className="font-medium">Teleprompting Service</p>
+                            <p className="text-sm text-gray-400">Personal Project + Collaborators</p>
+                        </div>
+                        <Link href="/" aria-label="link to workplace scheduling project page">
+                            <p className="text-gray-400 text-sm hover:underline cursor-not-allowed">View Project</p>
+                        </Link>
+                    </div>
+                </div>
             </motion.div>
         </div>
     )
---
+++ app/work/workplace-scheduling/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
+import { motion } from "framer-motion"
+import Image from "next/image";
+
+import main_image from '@/app/images/enter-phone-number.png';
+import main_page from '@/app/images/main_page.png'
+
+export default function Page() {
+    return (
+        <div>
+            <div className="sm:max-w-5xl w-11/12 mx-auto py-10">
+                <div className="pb-3 border-b flex flex-col space-y-1">
+                    <h1 className="font-semibold text-3xl">Workplace Scheduling (Shift List)</h1>
+                    <div>
+                        <p className="text-yellow-800 bg-yellow-100 rounded-full inline-block px-2 py-0.5 text-sm">In-progress</p>
+                    </div>
+                </div>
+                <motion.div className="flex flex-row space-x-1 mt-3" initial={{ y: 2, opacity: 0 }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 1 }}>
+                    <div className="group/div">
+                        <Avatar>
+                            <AvatarImage width={32} height={32} src="https://media.licdn.com/dms/image/D5603AQHyZFlBQNUheQ/profile-displayphoto-shrink_100_100/0/1695432106813?e=1704326400&v=beta&t=ONvKP4pzMEPiGxBRjj4uuje0WuK1yTNJRNVs72xG6Fs" alt="Collaborator"/>
+                            <AvatarFallback>DV</AvatarFallback>
+                        </Avatar>
+                        <div className="invisible group-hover/div:visible group-hover/div:opacity-100 opacity-0 mt-1 border px-2 py-0.5 absolute text-xs text-black rounded-full transition-all duration-300">
+                            Devin Vasavong
+                        </div>
+                    </div>
+                    <div className="group/div">
+                        <Avatar>
+                            <AvatarImage width={32} height={32} src="https://media.licdn.com/dms/image/D4D03AQEdRrV7BDJZnw/profile-displayphoto-shrink_100_100/0/1692653800547?e=1704326400&v=beta&t=nc943vp7qqTh4V1uCAMMqGRFQWwUXJ1lznDM7Flx8ms" alt="Collaborator"/>
+                            <AvatarFallback>AR</AvatarFallback>
+                        </Avatar>
+                        <div className="invisible group-hover/div:visible group-hover/div:opacity-100 opacity-0 mt-1 border px-2 py-0.5 absolute text-xs text-black rounded-full transition-all duration-300">
+                            Aidan Ryther
+                        </div>
+                    </div>
+                </motion.div>
+                <motion.div className="py-10" initial={{ y: 5, opacity: 0, }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 1.5 }} >
+                    <p>
+                        I wanted to create something that would benefit small businesses and their employees. I decided to create a scheduling system that is affordable and effective.
+                        <br/>
+                        <br/>
+                        Workplace scheduling software is usually expensive and hard to use. Shiftlist elimates both of those problems by providing a simple and easy to use interface.
+                    </p>
+                    <div className="text-xs italic text-gray-400 py-3">
+                        <Image src={main_image} alt="Image of the login page" />
+                        Displayed is an image of the login page
+                    </div>
+                    <p>
+                        Shiftlist is a web application that allows employees to view their schedule and request time off. Managers can create schedules, view employee requests, and approve/deny them.
+                        <br/>
+                        <br/>
+                        We are currently working on redesigning some of the pages and adding new features. We are also working on a mobile application that will allow employees to view their schedule and request time off.
+                    </p>
+                    <div className="text-xs italic text-gray-400 py-3">
+                        <Image src={main_page} alt="Image of the shifts page" />
+                        Displayed is an image of the shifts page
+                    </div>
+                </motion.div>
+            </div>
+        </div>
+    )
+}

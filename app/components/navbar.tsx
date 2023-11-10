bd57bbfb114d60538114b26847e0567886ddb323
Devin Vasavong <devin@vasavong.com>
2023-11-10 11:27:49 -0500

    Updated layout of project view

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
     return (
         <nav className="w-full">
             <motion.div transition={{ duration: 1}} initial={{ opacity: 0}} animate={{ opacity: 100 }} className="top-0 z-100 w-auto h-auto py-5 flex flex-row justify-start gap-10 bg-white/50 sm:bg-transparent border-black/5 border-b sm:border-none backdrop-blur-lg sm:backdrop-blur-none overflow-y-scroll">
-                <Link href="https://drive.google.com/file/d/1HGNj6WSvbUXVfA8lX-ULNBuWBJOPA3HC/view?usp=share_link" target="_blank" aria-label="link to my resume">
+                <Link href="https://cdn.vasavong.dev/Chanthanong_Vasavong_Resume.pdf" target="_blank" aria-label="link to my resume">
                     <div className="flex flex-col">
                         <h1 className="text-xs sm:text-sm">Resume</h1>
                         <p className="text-xs sm:text-sm text-gray-500">Click to view</p>
--- app/work/workplace-scheduling/page.tsx
+++ app/work/workplace-scheduling/page.tsx
@@ -17,46 +17,53 @@ export default function Page() {
                         <p className="text-yellow-800 bg-yellow-100 rounded-full inline-block px-2 py-0.5 text-sm">In-progress</p>
                     </div>
                 </div>
-                <motion.div className="flex flex-row space-x-1 mt-3" initial={{ y: 2, opacity: 0 }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 1 }}>
-                    <div className="group/div">
-                        <Avatar>
-                            <AvatarImage width={32} height={32} src="https://media.licdn.com/dms/image/D5603AQHyZFlBQNUheQ/profile-displayphoto-shrink_100_100/0/1695432106813?e=1704326400&v=beta&t=ONvKP4pzMEPiGxBRjj4uuje0WuK1yTNJRNVs72xG6Fs" alt="Collaborator"/>
-                            <AvatarFallback>DV</AvatarFallback>
-                        </Avatar>
-                        <div className="invisible group-hover/div:visible group-hover/div:opacity-100 opacity-0 mt-1 border px-2 py-0.5 absolute text-xs text-black rounded-full transition-all duration-300">
-                            Devin Vasavong
+                <motion.div className="flex flex-col space-y-1 mt-3" initial={{ y: 2, opacity: 0 }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 1 }}>
+                    <p className="text-gray-400 font-medium text-sm">Contributors</p>
+                    <div className="flex flex-row space-x-1">
+                        <div className="group/div">
+                            <Avatar>
+                                <AvatarImage width={32} height={32} src="https://media.licdn.com/dms/image/D5603AQHyZFlBQNUheQ/profile-displayphoto-shrink_100_100/0/1695432106813?e=1704326400&v=beta&t=ONvKP4pzMEPiGxBRjj4uuje0WuK1yTNJRNVs72xG6Fs" alt="Collaborator"/>
+                                <AvatarFallback>DV</AvatarFallback>
+                            </Avatar>
+                            <div className="invisible group-hover/div:visible group-hover/div:opacity-100 opacity-0 mt-1 border px-2 py-0.5 absolute text-xs text-black rounded-full transition-all duration-300">
+                                Devin Vasavong
+                            </div>
                         </div>
-                    </div>
-                    <div className="group/div">
-                        <Avatar>
-                            <AvatarImage width={32} height={32} src="https://media.licdn.com/dms/image/D4D03AQEdRrV7BDJZnw/profile-displayphoto-shrink_100_100/0/1692653800547?e=1704326400&v=beta&t=nc943vp7qqTh4V1uCAMMqGRFQWwUXJ1lznDM7Flx8ms" alt="Collaborator"/>
-                            <AvatarFallback>AR</AvatarFallback>
-                        </Avatar>
-                        <div className="invisible group-hover/div:visible group-hover/div:opacity-100 opacity-0 mt-1 border px-2 py-0.5 absolute text-xs text-black rounded-full transition-all duration-300">
-                            Aidan Ryther
+                        <div className="group/div">
+                            <Avatar>
+                                <AvatarImage width={32} height={32} src="https://media.licdn.com/dms/image/D4D03AQEdRrV7BDJZnw/profile-displayphoto-shrink_100_100/0/1692653800547?e=1704326400&v=beta&t=nc943vp7qqTh4V1uCAMMqGRFQWwUXJ1lznDM7Flx8ms" alt="Collaborator"/>
+                                <AvatarFallback>AR</AvatarFallback>
+                            </Avatar>
+                            <div className="invisible group-hover/div:visible group-hover/div:opacity-100 opacity-0 mt-1 border px-2 py-0.5 absolute text-xs text-black rounded-full transition-all duration-300">
+                                Aidan Ryther
+                            </div>
                         </div>
                     </div>
                 </motion.div>
-                <motion.div className="py-10" initial={{ y: 5, opacity: 0, }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 1.5 }} >
-                    <p>
-                        I wanted to create something that would benefit small businesses and their employees. I decided to create a scheduling system that is affordable and effective.
-                        <br/>
-                        <br/>
-                        Workplace scheduling software is usually expensive and hard to use. Shiftlist elimates both of those problems by providing a simple and easy to use interface.
-                    </p>
-                    <div className="text-xs italic text-gray-400 py-3">
-                        <Image src={main_image} alt="Image of the login page" />
-                        Displayed is an image of the login page
+                <motion.div className="py-10 flex-col" initial={{ y: 5, opacity: 0, }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 1.5 }} >
+                    <div className="flex flex-col md:flex-row space-x-3 items-start justify-center">
+                        <p className="md:w-1/2">
+                            I wanted to create something that would benefit small businesses and their employees. I decided to create a scheduling system that is affordable and effective.
+                            <br/>
+                            <br/>
+                            Workplace scheduling software is usually expensive and hard to use. Shiftlist elimates both of those problems by providing a simple and easy to use interface.
+                        </p>
+                        <div className="text-xs italic text-gray-400 py-3 md:w-1/2">
+                            <Image src={main_image} alt="Image of the login page" />
+                            Displayed is an image of the login page
+                        </div>
                     </div>
-                    <p>
-                        Shiftlist is a web application that allows employees to view their schedule and request time off. Managers can create schedules, view employee requests, and approve/deny them.
-                        <br/>
-                        <br/>
-                        We are currently working on redesigning some of the pages and adding new features. We are also working on a mobile application that will allow employees to view their schedule and request time off.
-                    </p>
-                    <div className="text-xs italic text-gray-400 py-3">
-                        <Image src={main_page} alt="Image of the shifts page" />
-                        Displayed is an image of the shifts page
+                    <div className="flex flex-col md:flex-row space-x-3 items-start justify-center">
+                        <p className="md:w-1/2">
+                            Shiftlist is a web application that allows employees to view their schedule and request time off. Managers can create schedules, view employee requests, and approve/deny them.
+                            <br/>
+                            <br/>
+                            We are currently working on redesigning some of the pages and adding new features. We are also working on a mobile application that will allow employees to view their schedule and request time off.
+                        </p>
+                        <div className="text-xs italic text-gray-400 py-3 md:w-1/2">
+                            <Image src={main_page} alt="Image of the shifts page" />
+                            Displayed is an image of the shifts page
+                        </div>
                     </div>
                     <div className="text-xs italic text-gray-400 py-3">
                         <Image src={"https://cdn.vasavong.dev/new_announcements_page.png"} width={1920} height={1080} alt="Image of the shifts page" />
